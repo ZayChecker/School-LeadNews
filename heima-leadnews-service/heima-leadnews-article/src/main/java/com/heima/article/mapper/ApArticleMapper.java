ab97ee7874c0d24a207ff06cca4c73fa071730ff
ZayChecker <zaychecker@163.com>
2024-10-10 22:37:30 +0800

    360

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleMapper.java
@@ -0,0 +1,16 @@
+package com.heima.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.article.pojos.ApArticle;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface ApArticleMapper extends BaseMapper<ApArticle> {
+
+    //加载文章列表    type：1-加载更多 2-加载最新
+    public List<ApArticle> loadArticleList(@Param("dto") ArticleHomeDto articleHomeDto, @Param("type") Short type);
+}
